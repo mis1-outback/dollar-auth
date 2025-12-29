9b977b9461f7a1603bf7d17b77a588d3e914f2a1
leesuin0710 <suin3559@gmail.com>
2025-12-29 15:52:45 +0900

    feat:auth initial settings - Add Spring Web and Eureka Discovery Client dependencies - Connect to config server and discovery server issue: #1

---
+++ src/test/java/outback/the_dollar/auth/AuthApplicationTests.java
@@ -0,0 +1,13 @@
+package outback.the_dollar.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
